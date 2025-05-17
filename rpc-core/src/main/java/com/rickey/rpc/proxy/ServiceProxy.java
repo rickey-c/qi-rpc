59169cd562b938bfa0523a44f2299e8e89e1f3e4
rickey-c <1604120019@qq.com>
2025-05-17 23:37:41 +0800

    style(project): 代码格式化和注释完善

--- rpc-core/src/main/java/com/rickey/rpc/proxy/ServiceProxy.java
+++ rpc-core/src/main/java/com/rickey/rpc/proxy/ServiceProxy.java
@@ -24,22 +24,18 @@ public class ServiceProxy implements InvocationHandler {
     private static final String SERVICE_URL = "http://localhost:8080";
 
     /**
-     * 
-     * @param proxy the proxy instance that the method was invoked on
-     *
+     * @param proxy  the proxy instance that the method was invoked on
      * @param method the {@code Method} instance corresponding to
-     * the interface method invoked on the proxy instance.  The declaring
-     * class of the {@code Method} object will be the interface that
-     * the method was declared in, which may be a superinterface of the
-     * proxy interface that the proxy class inherits the method through.
-     *
-     * @param args an array of objects containing the values of the
-     * arguments passed in the method invocation on the proxy instance,
-     * or {@code null} if interface method takes no arguments.
-     * Arguments of primitive types are wrapped in instances of the
-     * appropriate primitive wrapper class, such as
-     * {@code java.lang.Integer} or {@code java.lang.Boolean}.
-     *
+     *               the interface method invoked on the proxy instance.  The declaring
+     *               class of the {@code Method} object will be the interface that
+     *               the method was declared in, which may be a superinterface of the
+     *               proxy interface that the proxy class inherits the method through.
+     * @param args   an array of objects containing the values of the
+     *               arguments passed in the method invocation on the proxy instance,
+     *               or {@code null} if interface method takes no arguments.
+     *               Arguments of primitive types are wrapped in instances of the
+     *               appropriate primitive wrapper class, such as
+     *               {@code java.lang.Integer} or {@code java.lang.Boolean}.
      * @return 方法调用结果
      * @throws Throwable 序列化异常等
      */
--- rpc-core/src/main/java/com/rickey/rpc/registry/LocalRegistry.java
+++ rpc-core/src/main/java/com/rickey/rpc/registry/LocalRegistry.java
@@ -13,31 +13,34 @@ public class LocalRegistry {
     /**
      * 注册信息存储
      */
-    private static final Map<String,Class<?>> map = new ConcurrentHashMap<>();
+    private static final Map<String, Class<?>> MAP = new ConcurrentHashMap<>();
 
     /**
      * 向本地注册服务
+     *
      * @param serviceName 服务名
-     * @param implClass 实现类
+     * @param implClass   实现类
      */
-    public static void register(String serviceName,Class<?> implClass){
-        map.put(serviceName,implClass);
+    public static void register(String serviceName, Class<?> implClass) {
+        MAP.put(serviceName, implClass);
     }
 
     /**
      * 获取服务
+     *
      * @param serviceName 服务名
-     * @return
+     * @return 服务
      */
-    public static Class<?> get(String serviceName){
-        return map.get(serviceName);
+    public static Class<?> get(String serviceName) {
+        return MAP.get(serviceName);
     }
 
     /**
-     * 删除服务 
+     * 删除服务
+     *
      * @param serviceName 服务名
      */
-    public static void remove(String serviceName){
-        map.remove(serviceName);
+    public static void remove(String serviceName) {
+        MAP.remove(serviceName);
     }
 }
